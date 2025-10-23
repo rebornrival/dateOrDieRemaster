3b6d13070a3817226ce860436f9fbac8b9fb190a
Renee DeVivo <renee@devivo.com>
2025-10-22 22:23:50 -0700

    made toddrick intro

---
+++ date-or-die-remaster/scripts/prologue.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var eugene_run = false
+var toddrick_run = false
+
+func _ready() -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	if Globals.eugene == true and eugene_run == false:
+		$eugene/AnimationPlayer.play("tween")
+		eugene_run = true
+	elif Globals.eugene == false and eugene_run == false:
+		$toddrick/AnimationPlayer.play("tween")
+		eugene_run = true
+
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/character_select.tscn")
